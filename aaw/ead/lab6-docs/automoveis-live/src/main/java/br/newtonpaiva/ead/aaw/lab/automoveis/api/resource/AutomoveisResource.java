c36166253691d168dec6b03b69121a5a55a4036b
Waldir R. Pires Jr <waldir.pires@gmail.com>
2020-11-05 16:00:44 -0300

    LAb 6 Documentation

--- aaw/ead/lab6-docs/automoveis-live/src/main/java/br/newtonpaiva/ead/aaw/lab/automoveis/api/resource/AutomoveisResource.java
+++ aaw/ead/lab6-docs/automoveis-live/src/main/java/br/newtonpaiva/ead/aaw/lab/automoveis/api/resource/AutomoveisResource.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.newtonpaiva.ead.aaw.lab.automoveis.api.request.AutomovelRequest;
 import br.newtonpaiva.ead.aaw.lab.automoveis.api.response.AutomovelResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -32,34 +33,94 @@ public interface AutomoveisResource {
     @Operation(summary = "Recupera um automóvel apartir de um ID")
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "200", 
-                    description = "Automóvel encontrado.", 
+                    responseCode = "200",
+                    description = "Automóvel encontrado.",
                     content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AutomovelResponse.class)) }),
             @ApiResponse(
-                    responseCode = "400", 
-                    description = "ID inválido.", 
+                    responseCode = "400",
+                    description = "ID inválido.",
                     content = @Content),
             @ApiResponse(
-                    responseCode = "404", 
-                    description = "Automóvel não encontrado.", 
-                    content = @Content) 
+                    responseCode = "404",
+                    description = "Automóvel não encontrado.",
+                    content = @Content)
             }
     )
     @GetMapping(path = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<AutomovelResponse> getById(@PathVariable Optional<String> id);
 
+    @Operation(summary = "Criar um novo automóvel")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Automóvel criado com sucesso.",
+                    content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AutomovelResponse.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Requisição inválida.",
+                    content = @Content)
+            }
+    )
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<AutomovelResponse> create(
             @RequestBody(required = true) @Valid Optional<AutomovelRequest> request);
 
+    @Operation(summary = "Atualiza um automóvel apartir de um ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Automóvel atualizado.",
+                    content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AutomovelResponse.class)) }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Requisição inválida.",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Automóvel não encontrado.",
+                    content = @Content)
+            }
+    )
     @PutMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<AutomovelResponse> update(@PathVariable Optional<UUID> id,
             @RequestBody Optional<AutomovelRequest> request);
 
+    @Operation(summary = "Deleta um automóvel apartir de um ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Automóvel excluído."),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Automóvel não encontrado.",
+                    content = @Content)
+            }
+    )
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<UUID> deleteById(@PathVariable Optional<String> id);
 
+    @Operation(summary = "Recupera a listagem de automóveis")
+    @ApiResponses(
+        value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Lista de automóveis",
+                content = {
+                    @Content(
+                        mediaType = APPLICATION_JSON_VALUE,
+                        array = @ArraySchema(
+                            schema = @Schema(
+                                implementation = AutomovelResponse.class
+                            )
+                        )
+                    )
+                }
+            ),
+        }
+    )
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AutomovelResponse>> getAll();
 
